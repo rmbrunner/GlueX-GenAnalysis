8b588276fa0f3dd695c9151f6f8ece5d5087b686
rmbrunner <rmbrunner@fsu.edu>
2025-06-19 01:51:48 -0400

    First commit: add files to github

---
+++ Makefile
@@ -0,0 +1,38 @@
+#!gnumake
+
+INCLUDE = -I. -I../../$(BMS_OSNAME)/include -I$(HALLD_RECON_HOME)/$(BMS_OSNAME)/include -I/group/halld/Software/builds/$(BMS_OSNAME)/gluex_root_analysis/gluex_root_analysis-$(GLUEX_ROOT_ANALYSIS_VERSION)/$(BMS_OSNAME)/include/DSelector/
+ROOTFLAGS = $(shell $(ROOTSYS)/bin/root-config --cflags) 
+CXXFLAGS = -O -Wall -fPIC 
+FLAGS = $(CXXFLAGS) $(ROOTFLAGS) $(INCLUDE)
+ROOTGLIBS = $(shell $(ROOTSYS)/bin/root-config --glibs) 
+LIBS = $(ROOTGLIBS) -L/group/halld/Software/builds/$(BMS_OSNAME)/gluex_root_analysis/gluex_root_analysis-$(GLUEX_ROOT_ANALYSIS_VERSION)/$(BMS_OSNAME)/lib/ -lDSelector
+
+all: install
+
+install: build_it install_it
+
+build_it: mkdir_build ./bin/genAnalysis
+
+mkdir_build: 
+	@mkdir -p ./bin
+
+install_it: 
+	@mkdir -p ./bin 
+	@cp ./bin/genAnalysis ./bin/
+
+./bin/genAnalysis: genAnalysis.cc 
+	@rm -f ./bin/genAnalysis 
+	g++ $(FLAGS) genAnalysis.cc -o ./bin/genAnalysis $(LIBS) 
+	@chmod 755 ./bin/genAnalysis
+
+clean: 
+	@rm -rf ./bin
+	@rm -rf ./bin/genAnalysis
+
+env: 
+	@echo C++ $(C++)
+	@echo BMS_OSNAME $(BMS_OSNAME)
+	@echo INCLUDE $(INCLUDE) 
+	@echo FLAGS $(FLAGS)
+	@echo LIBS $(LIBS)
+
---
+++ genAnalysis.cc
@@ -0,0 +1,269 @@
+// CodeGen.cc
+// This program generates a C++ analysis file (e.g. AnalyzeD.cc) based on
+// ROOT RDataFrame and a list of selected branches in selectedBranches.csv.
+// It inspects an input ROOT file, computes histogram ranges and bin widths,
+// and writes out code that uses RDataFrame to create 1D and 2D histograms
+
+// (including Dalitz plots) with LaTeX axes, toggled error bars, and optimized
+// bins.
+
+#include <ROOT/RDataFrame.hxx>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TKey.h>
+#include <TLegend.h>
+#include <TTree.h>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <ostream>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace ROOT;
+using std::map;
+using std::set;
+using std::string;
+using std::vector;
+
+string particleNameToLatex(const string &branch) {
+  static const map<string, string> particleMap = {
+      {"PiMinus", "#pi^{-}"}, {"PiPlus", "#pi^{+}"}, {"KMinus", "K^{-}"},
+      {"KPlus", "K^{+}"},     {"Proton", "p"},       {"AntiProton", "#bar{p}"},
+      {"Neutron", "n"},       {"Electron", "e^{-}"}, {"Positron", "e^{+}"},
+      {"Photon", "#gamma"}};
+
+  string s = branch;
+  if (s.rfind("mass_", 0) == 0)
+    s = s.substr(5);
+
+  std::stringstream ss(s);
+  string segment;
+  vector<string> particles;
+  while (std::getline(ss, segment, '_')) {
+    std::regex re("([A-Za-z]+)([0-9]*)$");
+    std::smatch match;
+    if (std::regex_match(segment, match, re)) {
+      string base = match[1];
+      string index = match[2];
+      auto it = particleMap.find(base);
+      string latex = (it != particleMap.end()) ? it->second : base;
+      if (!index.empty())
+        latex += "_{" + index + "}";
+      particles.push_back(latex);
+    }
+  }
+  string joined;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    if (i)
+      joined += " ";
+    joined += particles[i];
+  }
+  return joined;
+}
+
+size_t knuthBins(Long64_t n) {
+  return static_cast<size_t>(std::ceil(2.0 * std::pow(double(n), 2.0 / 5.0)));
+}
+
+string makeHisto1D(const string &b, size_t bins, double min, double max,
+                   bool showErrors) {
+  std::ostringstream out;
+  double binWidth = static_cast<int>(1000 * ((max - min) / bins));
+  string histName = "h1_" + b;
+  string title = "Mass[" + particleNameToLatex(b) + "] (GeV)";
+  out << "  auto " << histName << " = df.Histo1D({\"" << histName << "\",\""
+      << "" << "\"," << bins << "," << min << "," << max << "}, \"" << b
+      << "\");\n";
+  out << histName << "->GetXaxis()->SetTitle(\"" << title << "\");"
+      << std::endl;
+  out << histName << "->GetYaxis()->SetTitle(\"Counts / " << binWidth
+      << " MeV\");" << std::endl;
+  out << histName << "->Write();\n";
+
+  // if (!showErrors)
+  // out << "  " << histName << "->SetErrorOption(\"\");\n";
+  return out.str();
+}
+
+string makeHisto2D(const string &x, const string &y, size_t bx, double xmin,
+                   double xmax, size_t by, double ymin, double ymax,
+                   const string &xlabel, const string &ylabel,
+                   bool showErrors, string dfName) {
+  std::ostringstream out;
+  string histName = "h2_" + x + "_" + y;
+  string title = xlabel + " vs. " + ylabel;
+  out << "  auto " << histName << " = " << dfName << ".Histo2D({\"" << histName << "\",\""
+      << "" << "\"," << bx << "," << xmin << "," << xmax << "," << by << ","
+      << ymin << "," << ymax << "}, \"" << x << "\", \"" << y << "\");\n";
+  out << histName << "->GetXaxis()->SetTitle(\"" << xlabel << "\");"
+      << std::endl;
+  out << histName << "->GetYaxis()->SetTitle(\"" << ylabel << "\");"
+      << std::endl;
+  out << histName << "->Write();\n";
+  // if (!showErrors)
+  // out << "  " << histName << "->SetErrorOption(\"\");\n";
+  return out.str();
+}
+
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    std::cerr
+        << "Usage: " << argv[0]
+        << " <input.root> <selectedBranches.csv> <output.cc> [showErrors=0]\n";
+    return 1;
+  }
+  string inputFile = argv[1];
+
+  string csvFile = argv[2];
+  string outFile = argv[3];
+  bool showErrors = (argc > 4 ? std::stoi(argv[4]) : 0);
+
+  std::ifstream ifs(csvFile);
+  vector<TString> sel;
+  std::string line;
+  while (std::getline(ifs, line)) {
+    if (!line.empty()) {
+      line.erase(line.find_last_not_of(" \n\r\t") + 1);
+      sel.push_back(line);
+    }
+  }
+
+  TFile *f = TFile::Open(inputFile.c_str(), "READ");
+  if (!f || f->IsZombie()) {
+    std::cerr << "ERROR: Cannot open file " << inputFile << "\n";
+    return 1;
+  }
+  TIter itKey(f->GetListOfKeys());
+  TKey *key;
+  string treeName;
+  while ((key = (TKey *)itKey())) {
+    if (std::string(key->GetClassName()) == "TTree") {
+      treeName = key->GetName();
+      break;
+    }
+  }
+  if (treeName.empty()) {
+    std::cerr << "ERROR: No TTree found in file.\n";
+    return 1;
+  }
+
+  RDataFrame df(treeName, inputFile);
+  auto allCols = df.GetColumnNames();
+
+  vector<string> cols;
+  for (const auto &b : allCols) {
+    TString tmp = b;
+    for (auto &i : sel) {
+      TString thing = i;
+
+      if (i == tmp) {
+        cols.push_back(b);
+        break;
+      }
+    }
+
+  }
+
+  vector<string> massCols, angleCols;
+  for (auto &b : cols) {
+    if (b.find("costh") != string::npos || b.find("phi") != string::npos)
+      angleCols.push_back(b);
+    else
+      massCols.push_back(b);
+  }
+
+  std::ofstream ofs(outFile);
+
+  ofs << "#include <ROOT/RDataFrame.hxx>\n"
+      << "#include <TFile.h>\n"
+      << "#include <TCanvas.h>\n" 
+      << "#include <TKey.h>\n"
+      << "using namespace ROOT;\n"
+      << "void plots(){\n"
+      << "  TFile *f = TFile::Open(\"" << inputFile << "\", \"READ\");\n"
+      << "  if (!f || f->IsZombie()) throw std::runtime_error(\"Cannot open \\\" "
+      << inputFile <<"\\\"\");\n"
+      << "  TIter itKey(f->GetListOfKeys()); TKey *key; std::string tree;\n"
+      << "  while ((key = (TKey*)itKey())) {\n    if "
+         "(std::string(key->GetClassName()) == \"TTree\") { tree = "
+         "key->GetName(); break; }\n  }\n"
+      << "  if (tree.empty()) throw std::runtime_error(\"No TTree\");\n"
+      << "  auto df = RDataFrame(tree, \"" << inputFile << "\");\n"
+      << "TFile* histograms =TFile::Open(\"histograms.root\", \"RECREATE\"); \n"
+      << "histograms->cd();\n";
+  std::cout << "Creating 1D Histograms..." << std::endl;
+  for (auto &b : massCols) {
+    double minv = df.Min<double>(b).GetValue();
+    double maxv = df.Max<double>(b).GetValue();
+    Long64_t n = df.Count().GetValue();
+    size_t bins1 = knuthBins(n);
+    ofs << makeHisto1D(b, bins1, minv, maxv, showErrors);
+  }
+  std::cout << "Creating 2D Mass Correlation Plots..." << std::endl;
+  for (size_t i = 0; i < massCols.size(); ++i) {
+    for (size_t j = i + 1; j < massCols.size(); ++j) {
+      string dfName = "df";
+      auto &x = massCols[i];
+      auto &y = massCols[j];
+      double xmin = df.Min<double>(x).GetValue();
+      double xmax = df.Max<double>(x).GetValue();
+      double ymin = df.Min<double>(y).GetValue();
+      double ymax = df.Max<double>(y).GetValue();
+      Long64_t n = df.Count().GetValue();
+      size_t bx = knuthBins(n), by = knuthBins(n);
+      string xlab = "Mass[" + particleNameToLatex(x) + "] (GeV)";
+      string ylab = "Mass[" + particleNameToLatex(y) + "] (GeV)";
+      ofs << makeHisto2D(x, y, bx, xmin, xmax, by, ymin, ymax, xlab, ylab,
+                         showErrors, dfName);
+    }
+  }
+
+  std::cout << "Creating Angular Distribution Plots..." << std::endl;
+  for (auto &ang : angleCols) {
+    for (auto &m : massCols) {
+      string dfName = "df";
+      double xmin = df.Min<double>(m).GetValue();
+      double xmax = df.Max<double>(m).GetValue();
+      double ymin = df.Min<double>(ang).GetValue();
+      double ymax = df.Max<double>(ang).GetValue();
+      Long64_t n = df.Count().GetValue();
+      size_t bm = knuthBins(n), ba = knuthBins(n);
+      string xtitle = "Mass[" + particleNameToLatex(m) + "] (GeV)";
+      string ytitle = particleNameToLatex(ang);
+      ofs << makeHisto2D(m, ang, bm, xmin, xmax, ba, ymin, ymax, xtitle, ytitle,
+                         showErrors, dfName);
+    }
+  }
+  std::cout << "Creating 2D Dalitz Plots..." << std::endl;
+  unsigned long int counter = 0;
+  for (size_t i = 0; i < massCols.size(); ++i) {
+    for (size_t j = i + 1; j < massCols.size(); ++j) {
+      auto &b1 = massCols[i], &b2 = massCols[j];
+      string sq1 = b1 + "_sq";
+      string sq2 = b2 + "_sq";
+      string dfName = "df2_" + std::to_string(counter);
+      ofs << "  auto df2_" << counter << " = df.Define(\"" << sq1
+          << "\",[](double x){return x*x;}, {\"" << b1 << "\"}).Define(\"" << sq2
+          << "\",[](double x){return x*x;}, {\"" << b2 << "\"});\n";
+      double min1 = df.Min<double>(b1).GetValue();
+      double max1 = df.Max<double>(b1).GetValue();
+      double min2 = df.Min<double>(b2).GetValue();
+      double max2 = df.Max<double>(b2).GetValue();
+      Long64_t n = df.Count().GetValue();
+      size_t bins = knuthBins(n);
+      string xtitle = "Mass[" + particleNameToLatex(b1) + "]^{2} (GeV^{2})";
+      string ytitle = "Mass[" + particleNameToLatex(b2) + "]^{2} (GeV^{2})";
+      ofs << makeHisto2D(sq1, sq2, bins, min1 * min1, max1 * max1, bins,
+
+                         min2 * min2, max2 * max2, xtitle, ytitle, showErrors, dfName);
+      counter++;
+    }
+  }
+  ofs << "histograms->Close();\n";
+  ofs << "}";
+}
